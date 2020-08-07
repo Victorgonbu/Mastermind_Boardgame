f34406710d485d642cbaf84e03cbefff1e4f1610
victorG <peter200970@hotmail.com>
2020-08-06 19:41:20 -0500

    compare method added to classes

--- lib/code_braker.rb
+++ lib/code_braker.rb
@@ -11,4 +11,8 @@ class CodeBraker
 
     
 
+
+
+    
+
 end
--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,11 +1,31 @@
 class CodeMaker
     @@array = ["blue", "red", "yellow", "gray", "white", "black"]
     @@new_array = []
+ 
+    attr_reader :red_pegs, :white_pegs
+
     def self.scramble
       @@array.shuffle!
       4.times do 
         @@new_array << @@array.pop
       end
       @@new_array
+    end
+
+    def  compare (array)
+      @array = array
+      @red_pegs = 0
+      @white_pegs  = 0
+      
+      @array.each_with_index do |element, index|
+        @@new_array.each_with_index do |element2, index2|
+          if element == element2  && index == index2
+            @red_pegs += 1
+
+          elsif element == element2
+           @white_pegs += 1
+          end
+        end 
+      end
     end
 end
