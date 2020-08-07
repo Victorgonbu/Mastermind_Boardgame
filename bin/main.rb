25d7b893240af05a75412be4ed842c8d33731ae0
victorG <peter200970@hotmail.com>
2020-08-06 19:40:33 -0500

    Mastermind game now functional with ia

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,6 @@
 require_relative '../lib/code_maker.rb'
 require_relative '../lib/code_braker.rb'
+validate = 1
 puts 'Hello boys this is Mastermind board game i hope you enjoy it'
 
 puts 'Please let us know your name'
@@ -11,5 +12,19 @@ guest_variable = gets.chomp
 
 player = CodeBraker.new(name)
 player.create_array = guest_variable
-p player.array
-CodeMaker.scramble
+p CodeMaker.scramble
+ia = CodeMaker.new
+
+while validate < 13
+ia.compare(player.array)
+puts "red pegs: #{ia.red_pegs}, white_pegs #{ia.white_pegs}"
+
+if ia.red_pegs == 4
+    puts "Congratulations you guest it in #{validate} attempts"
+    validate = 12
+else 
+    puts "please make yor next attempt #{validate}"
+    player.create_array = gets.chomp
+end
+validate += 1
+end
