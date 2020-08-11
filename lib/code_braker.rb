4acbb0c8b7c2ec7e5343ab6448bbfde27102b25d
victorG <peter200970@hotmail.com>
2020-08-11 01:12:46 -0500

    rubocop errors fixed

--- lib/code_braker.rb
+++ lib/code_braker.rb
@@ -1,12 +1,11 @@
 class CodeBraker
-    attr_accessor :name, :array
-    
-    def initialize (name)
-        self.name = name
-    end
+  attr_accessor :name, :array
 
-    def create_array=(string)
-        self.array = string.split(" ")
-    end
+  def initialize(name)
+    self.name = name
+  end
 
+  def create_array=(string)
+    self.array = string.split(' ')
+  end
 end
--- lib/code_maker.rb
+++ lib/code_maker.rb
@@ -1,48 +1,50 @@
 class CodeMaker
-    @@array = ["blue", "red", "yellow", "gray", "white", "black"]
-    @@new_array = []
- 
-    attr_reader :red_pegs, :white_pegs
+  @array = %w[blue red yellow green white black]
+  @new_array = []
 
-    def self.scramble
-      @@array.shuffle!
-      4.times do 
-        @@new_array << @@array.pop
-      end
-      @@new_array
+  attr_reader :red_pegs, :white_pegs
+
+  def self.scramble
+    @@array.shuffle!
+    4.times do
+      @@new_array << @@array.pop
     end
+    @@new_array
+  end
 
-    def  compare (array)
-      @array = array
-      @red_pegs = 0
-      @white_pegs  = 0
-      
-      @array.each_with_index do |element, index|
-        @@new_array.each_with_index do |element2, index2|
-          if element == element2  && index == index2
-            @red_pegs += 1
+  def compare(array)
+    @array = array
+    @red_pegs = 0
+    @white_pegs = 0
 
-          elsif element == element2
-           @white_pegs += 1
-          end
-        end 
-      end
-    end
+    @array.each_with_index do |element, index|
+      @@new_array.each_with_index do |element2, index2|
+        if element == element2 && index == index2
+          @red_pegs += 1
 
-    def validate (player)
-      validate = 2
-      while validate <= 13
-        self.compare(player.array)
-        puts "red pegs : #{self.red_pegs}, white pegs : #{self.white_pegs}"
-        if self.red_pegs == 4 
-          puts "Congratulations you guest it in #{validate-1} attempts"
-          validate = 13
-        elsif validate < 13
-          puts "please make yor next attempt #{validate}"
-           player.create_array = gets.chomp
+        elsif element == element2
+          @white_pegs += 1
         end
-        puts "#{player.name} you ran out of turns :c, IA WINS. TOTAL POINTS: IA : 13 #{player.name} : 0" if validate == 13 && self.red_pegs != 4
-        validate += 1
       end
     end
+  end
+
+  def validate(player)
+    validate = 2
+    while validate <= 13
+      compare(player.array)
+      puts "red pegs : #{red_pegs}, white pegs : #{white_pegs}"
+      if red_pegs == 4
+        puts "Congratulations you guest it in #{validate - 1} attempts"
+        validate = 13
+      elsif validate < 13
+        puts "please make yor next attempt #{validate}"
+        player.create_array = gets.chomp
+      end
+      if validate == 13 && red_pegs != 4
+        puts "#{player.name} you ran out of turns :c, IA WINS. TOTAL POINTS: IA : 13 #{player.name} : 0"
+      end
+      validate += 1
+    end
+  end
 end
